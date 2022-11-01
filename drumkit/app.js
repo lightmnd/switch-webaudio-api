49070b0dcb7dd5b799d9e4d4ede1ca049a4e6d40
lightmnd <booking.djonly@gmail.com>
2022-11-01 15:40:43 +0100

    add env variables to hide url for sounds stored in S3 public bucket

--- drumkit/app.js
+++ drumkit/app.js
@@ -1,59 +1,49 @@
+const dotenv = require("dotenv");
+
 let allButtons = document.querySelectorAll(".pad");
 let audio;
 
 allButtons.forEach((btn, index) => {
-    btn.addEventListener("click", () => {
-        switch (index) {
-            case 0:
-                // Kick
-                audio = new Audio(
-                    "https://drumkit-sound-samples.s3.eu-west-3.amazonaws.com/kick.wav"
-                );
-                audio.preload = "auto";
-                audio.play();
-                break;
-            case 1:
-                // Snare
-                audio = new Audio(
-                    "https://drumkit-sound-samples.s3.eu-west-3.amazonaws.com/new_snare.wav"
-                );
-                audio.preload = "auto";
-                audio.play();
-                break;
-            case 2:
-                // Closed-HHat
-                audio = new Audio(
-                    "https://drumkit-sound-samples.s3.eu-west-3.amazonaws.com/closed-hihat.mp3"
-                );
-                audio.preload = "auto";
-                audio.play();
-                break;
-            case 3:
-                // Clap
-                audio = new Audio(
-                    "https://drumkit-sound-samples.s3.eu-west-3.amazonaws.com/descructured_clap.wav"
-                );
-                audio.preload = "auto";
-                audio.play();
-                break;
-            case 4:
-                // Open-HHat
-                audio = new Audio(
-                    "https://drumkit-sound-samples.s3.eu-west-3.amazonaws.com/open-hihat.mp3"
-                );
-                audio.preload = "auto";
-                audio.play();
-                break;
-            case 5:
-                // Tom
-                audio = new Audio(
-                    "https://drumkit-sound-samples.s3.eu-west-3.amazonaws.com/tom_808.wav"
-                );
-                audio.preload = "auto";
-                audio.play();
-                break;
-            default:
-                console.log("err");
-        }
-    });
+  btn.addEventListener("click", () => {
+    switch (index) {
+      case 0:
+        // Kick
+        audio = new Audio(process.env.KICK);
+        audio.preload = "auto";
+        audio.play();
+        break;
+      case 1:
+        // Snare
+        audio = new Audio(process.env.SNARE);
+        audio.preload = "auto";
+        audio.play();
+        break;
+      case 2:
+        // Closed-HHat
+        audio = new Audio(process.env.CLOSED_HIHAT);
+        audio.preload = "auto";
+        audio.play();
+        break;
+      case 3:
+        // Clap
+        audio = new Audio(process.env.CLAP);
+        audio.preload = "auto";
+        audio.play();
+        break;
+      case 4:
+        // Open-HHat
+        audio = new Audio(process.env.OPEN_HIHAT);
+        audio.preload = "auto";
+        audio.play();
+        break;
+      case 5:
+        // Tom
+        audio = new Audio(process.env.TOM);
+        audio.preload = "auto";
+        audio.play();
+        break;
+      default:
+        console.log("err");
+    }
+  });
 });
